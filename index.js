9dad9b98dfca0416af0cc1d3727d8ddee8e25ad3
Xelyfer <Xelyfer@gmail.com>
2021-08-11 01:27:48 +1200

    Finished game

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+function randomizeDices() {
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+}
+
+function changeDiceImage() {
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+}
+
+function whoWon() {
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+  } else {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+  }
+}
+
+function play() {
+  randomizeDices();
+  changeDiceImage();
+  whoWon();
+}
